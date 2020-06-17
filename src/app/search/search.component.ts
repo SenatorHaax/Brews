00d489243c4f429357c187c32ce1d9db3c48285b
Rasmus <filurt@hotmail.com>
2020-06-17 14:24:30 +0200

    working on autocomplete kinda working

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -59,31 +59,4 @@ export class SearchComponent implements OnInit {
         this.brews = this.filteredBrews;
       });
   }
-
-
-  // brews: any;
-  // searchText: string;
-  //
-  //
-  // constructor(private _http: HttpService) {
-  // }
-  //
-  // removeItems(name: string){
-  //   if (name){
-  //     this._http.getSpecefikBeerSearch(name).subscribe( data => {
-  //       this.brews = data;
-  //     });
-  //   }
-  // }
-  //
-  // clearSelection(): void {
-  //   window.location.reload();
-  // }
-  //
-  // ngOnInit(): void {
-  //     this._http.getBeer().subscribe(data => {
-  //         this.brews = data;
-  //       }
-  //     );
-  // }
 }
